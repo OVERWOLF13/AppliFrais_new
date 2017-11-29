dd6b4715318680df2d75cb6bd1dcb3b57d86528b
Lombard <Lombard@B4-W7-12.sio.chaptalqper>
2017-11-29 13:30:06 +0100

    merge t après modif

--- application/views/templates/t_comptable.php
+++ application/views/templates/t_comptable.php
@@ -61,7 +61,10 @@ $v_path = base_url('application/views');
 			<?php echo $body; ?>
 
 			<div id="pied">
-				<br></br>
+				<p>
+			    <a href="http://validator.w3.org/check?uri=referer"><img
+			      src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
+			  	</p>
 			</div>
 
 		</div>    
